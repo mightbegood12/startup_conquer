f46f9ba06935a7a7ab9af1b5c6c57472b5de41db
saiprasana1 <saiprasana2404@gmail.com>
2024-12-30 20:55:17 +0530

    about page

--- startupConquer/game/urls.py
+++ startupConquer/game/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('game/', views.game, name="game"),
     path('about/', views.about, name="about"),
+    path('login/', views.login, name="login"),
     path('', views.home, name="home")
 ]
--- startupConquer/game/views.py
+++ startupConquer/game/views.py
@@ -9,3 +9,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def login(request):
+    return render(request, 'login.html')
