e897fd84f0025ba07227dd219b046b33f659141d
mightbegood12 <banunaresh1976@gmail.com>
2024-12-31 00:46:02 +0530

    [ADD] Game Overview

--- startupConquer/static/js/home_script.js
+++ startupConquer/static/js/home_script.js
@@ -1,4 +1,5 @@
 const menu_item = document.getElementById("menu-item");
+const dd_content = document.getElementById("dd-content");
 function showMenu() {
   menu_item.classList.toggle("open");
   var div = document.getElementById("nav-btn"),
@@ -14,3 +15,22 @@ function showMenu() {
 }
 
 var rotated = false;
+
+function showDropdown(event, contentId, imgId) {
+  const dd_content = document.getElementById(contentId);
+  const dd_img = document.getElementById(imgId);
+  if (dd_content.style.visibility === "visible") {
+    dd_content.style.visibility = "hidden";
+    dd_content.style.height = "0";
+    dd_content.style.opacity = "0";
+  } else {
+    dd_content.style.visibility = "visible";
+    dd_content.style.height = "fit-content"; // You can set a specific height if you prefer
+    dd_content.style.opacity = "1";
+  }
+  // dd_content.classList.toggle("open");
+  var deg = rotated ? 0 : 180;
+  dd_img.style.transform = "rotate(" + deg + "deg)";
+
+  rotated = !rotated;
+}
