dfc6c59e8aa952ec66418b7ebf9f6e76e1d2ba0c
mightbegood12 <banunaresh1976@gmail.com>
2025-06-06 17:58:16 +0530

    [FIX] Django Tailwind Config version

--- startupConquer/build.sh
+++ startupConquer/build.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-# build.sh
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Build Tailwind CSS for production
 python manage.py tailwind build
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Run DB migrations
+python manage.py migrate
